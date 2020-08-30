7a3a32e2b0f15e0adebe15984923cb03ec633793
Bius <guilhermebrechot@gmail.com>
2020-08-30 17:43:03 -0300

    Terminada a interface simples
    Terminado interface, algumas funções não estão funcionando no momento

--- python/app.py
+++ python/app.py
@@ -8,32 +8,41 @@ app = Flask(__name__, static_folder='../Static', template_folder='../templates')
 #Funcoes
 def get_rest(link):
     chamar = requests.get(link)
-    return chamar.json()
+    chamar = chamar.json()
+    if type(chamar) is dict:
+        chamar = [chamar]
+    return chamar
 
 #Paginas
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html", titulo=titulo, css=css, js=js, categorias=categorias, versao=versao)
+    return render_template("index.html", titulo=titulo, css=css, js=js, tabelas=tabelas, versao=versao)
 
 @app.route("/controle", methods=['GET', 'POST'])
 def controle():
     conteudo=""
+    setores=""
+    fornecedor=""
+    categorias=""
     listar = request.form.get("listar")
     if listar is not None:
-        uri = "https://smi-2020.herokuapp.com/{}/".format(listar)
+        uri = "{}/{}".format(link_fonte, listar)
         expecifico = request.form.get("procurar")
         if expecifico is not None:
-            uri = "{}{}".format(uri, expecifico)
+            uri = "{}/{}".format(uri, expecifico)
         conteudo = get_rest(uri)
-    return render_template("controle.html", titulo=titulo, css=css, js=js, categorias=categorias, listar=listar, conteudo=conteudo, versao=versao)
-
+        #Para listas
+        setores = get_rest("{}/setorFuncionarios".format(link_fonte))
+        categorias = get_rest("{}/categorias".format(link_fonte))
+        fornecedor = get_rest("{}/fornecedores".format(link_fonte))
+    return render_template("controle.html", titulo=titulo, css=css, js=js, tabelas=tabelas, listar=listar, conteudo=conteudo, fornecedor=fornecedor, setores=setores, categorias=categorias, versao=versao)
 
 @app.route("/criar", methods=['GET', 'POST'])
 def criar():
 
     listar = request.form.get("listar")
-    uri = "https://smi-2020.herokuapp.com/{}/".format(listar)
+    uri = "{}/{}/".format(link_fonte, listar)
 
     if listar == "categorias":
         categoria_add = request.form.get("categoria_add")
@@ -44,21 +53,39 @@ def criar():
         nome_cnpj_add = request.form.get("nome_cnpj_add")    
         data = {'cnpj':'{}'.format(cnpj_add), 'id':0, 'nome':'{}'.format(nome_cnpj_add)}
 
-    elif listar == "funcioanarios":
-        pass
-
-    elif listar == "produros":
-        pass
+    elif listar == "funcionarios":
+        nome_add = request.form.get("nome_add")    
+        sobrenome_add = request.form.get("sobrenome_add")   
+        setores_add = request.form.get("setores_add")   
+        usuario_add = "null"
+        data = {"id": 0, "nome": "{}".format(nome_add), "setorId": "{}".format(setores_add), "sobreNome": "{}".format(sobrenome_add), "usuarioId": "{}".format(usuario_add)}
+    
+    elif listar == "lotes":
+        #Não funcionando
+        entrega_add = request.form.get("entrega_add")    
+        valor_add = request.form.get("valor_add")   
+        fornecedor_add = request.form.get("fornecedor_add")  
+        data = {"dataDeEntrega": "{}".format(entrega_add), "fornecedor": "{}".format(fornecedor_add), "id": 0, "valorCompra": "{}".format(valor_add)}
+    
+    elif listar == "produtos":
+        #Não funcionando
+        nome_add = request.form.get("nome_add")    
+        marca_add = request.form.get("marca_add")    
+        descricao_add = request.form.get("descricao_add")    
+        preco_add = request.form.get("preco_add")    
+        categoria_add = request.form.get("categoria_add")    
+        fornecedor_add = request.form.get("fornecedor_add")    
+        data = {"categoria": "{}".format(categoria_add), "descricao": "{}".format(descricao_add), "fornecedores": "{}".format(fornecedor_add), "id": 0, "marca": "{}".format(marca_add), "nome": "{}".format(nome_add), "preco": "{}".format(preco_add)}
 
     elif listar == "setorFuncionarios":
         setorFuncionarios_add = request.form.get("setorFuncionarios_add")
         data = {'id':0,'nome':'{}'.format(setorFuncionarios_add)}
+
     else:
         pass
 
     headers = {'Content-type': 'application/json'}
     data_json = json.dumps(data)
-    print(data_json)
     requests.post(uri, data=data_json, headers=headers)
 
     return redirect(url_for("index")) 
@@ -68,7 +95,7 @@ def criar():
 def atualizar():
 
     listar = request.form.get("listar")
-    uri = "https://smi-2020.herokuapp.com/{}/".format(listar)
+    uri = "{}/{}/".format(link_fonte, listar)
 
     if listar == "categorias":
         categoria_id_alt = request.form.get("categoria_id_alt")
@@ -82,12 +109,35 @@ def atualizar():
         fornecedor_nome_atl = request.form.get("fornecedor_nome_atl")
         uri=uri + "{}".format(fornecedor_id_atl)
         data = {'cnpj':'{}'.format(fornecedor_cnpj_atl),'id':'{}'.format(fornecedor_id_atl),'nome':'{}'.format(fornecedor_nome_atl)}
-        
-    elif listar == "funcioanarios":
-        pass
+
+    elif listar == "funcionarios":
+        funcionario_id_atl = request.form.get("funcionario_id_atl")
+        nome_alt = request.form.get("nome_alt")    
+        sobrenome_alt = request.form.get("sobrenome_alt")
+        setores_alt = request.form.get("setores_alt")   
+        usuario_alt = "null"
+        uri=uri + "{}".format(funcionario_id_atl)
+        data = {"id": "{}".format(funcionario_id_atl), "nome": "{}".format(nome_alt), "usuarioId": "{}".format(usuario_alt), "setor": "{}".format(setores_alt), "sobreNome": "{}".format(sobrenome_alt)}
+
+    elif listar == "lotes":
+        lote_id_atl = request.form.get("lote_id_atl")
+        entrega_alt = request.form.get("entrega_alt")
+        valor_alt = request.form.get("valor_alt")
+        fornecedor_alt = request.form.get("fornecedor_alt")
+        uri=uri + "{}".format(lote_id_atl)
+        data = {"dataDeEntrega": "{}".format(entrega_alt), "fornecedor": "{}".format(fornecedor_alt), "id": "{}".format(lote_id_atl),"valorCompra": "{}".format(valor_alt)}
 
     elif listar == "produros":
-        pass
+        #Nao funciona
+        produto_id_atl = request.form.get("produto_id_atl")
+        nome_alt = request.form.get("nome_alt")
+        marca_alt = request.form.get("marca_alt")
+        preco_alt = request.form.get("preco_alt")
+        desc_alt = request.form.get("desc_alt")
+        fornecedor_alt = request.form.get("fornecedor_alt")
+        categoria_alt = "null"
+        uri=uri + "{}".format(produto_id_atl)
+        data = {"categoria": "{}".format(categoria_alt), "descricao": "{}".format(desc_alt), "fornecedores": "{}".format(fornecedor_alt), "id": "{}".format(produto_id_atl), "marca": "{}".format(marca_alt), "nome": "{}".format(nome_alt), "preco": "{}".format(preco_alt)}
 
     elif listar == "setorFuncionarios":
         setor_id_alt = request.form.get("setor_id_alt")
@@ -96,6 +146,7 @@ def atualizar():
         data = {'id':'{}'.format(setor_id_alt),'nome':'{}'.format(setor_nome_alt)}
     else:
         pass
+
 
     headers = {'Content-type': 'application/json'}
     data_json = json.dumps(data)
@@ -109,7 +160,7 @@ def atualizar():
 def deletar():
 
     listar = request.form.get("listar")
-    uri = "https://smi-2020.herokuapp.com/{}/".format(listar)
+    uri = "{}/{}/".format(link_fonte, listar)
     
     deletar_valor = request.form.get("deletar_valor")
 
@@ -127,6 +178,7 @@ if __name__ == "__main__":
     titulo = "SMI - Sistema de supermercado inteligente"
     css = ["./Static/css/reset.css", "./Static/css/bootstrap.css", "./Static/css/css_pessoal.css"]
     js = ["./Static/js/jquery-3.4.1.slim.min.js", "./Static/js/bootstrap.js", "./Static/js/popper.min.js", "./Static/js/js_pessoal.js"]
-    categorias = ["categorias", "fornecedores", "funcionarios", "produtos", "setorFuncionarios"]
-    versao = "0.0.1 - SMI"
+    link_fonte = "https://smi-2020.herokuapp.com"
+    tabelas = ["categorias", "fornecedores", "funcionarios", "lotes", "produtos", "setorFuncionarios"]
+    versao = "0.0.2 - SMI - Continuando o projeto"
     app.run(host='0.0.0.0', debug=True)
